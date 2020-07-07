ee3de2e898ce9cf0cc5bc0339ac096bfb25efa62
Robinson Montes <1574@holbertonschool.com>
2020-07-07 01:17:44 +0000

    again

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.\w*)\]\s\[to:(\W*\d*)\]\s\[flags:((?:-?\d:?)+)/).join(separator=', ')
+puts ARGV[0].scan(/\[from:(.\w*)\]\s\[to:(\W*\d*)\]\s\[flags:((?:-?\d:?)+)/).join(separator=',')
