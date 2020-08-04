d112158189e9d2169386bad2f128592ad0a112cc
Robinson Montes <1574@holbertonschool.com>
2020-08-04 21:57:19 +0000

    Make changes to a server

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,9 @@
-#!/usr/bin/env bash
+# Puppet to set changes in a server
+file_line { 'disable password login':
+  path => 'etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'add path to find the keys':
+  path => 'etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
