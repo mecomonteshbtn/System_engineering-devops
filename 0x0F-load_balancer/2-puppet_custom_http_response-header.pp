4f903b43d7f1ccedffeb4cf18360c0fcf5b92145
Robinson Montes <1574@holbertonschool.com>
2020-08-15 01:54:49 +0000

    first attempt with pupptes

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,1 +1,16 @@
-#!/usr/bin/env bash
+# Install and configure nginx on a ubuntu 16 server
+
+exec { 'apt-get update':
+  command => "apt-get update"
+  path    => "/usr/bin:/usr/sbin:/bin:/sbin",
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt update'],
+}
+
+exec { 'custom_header':
+  command => "sed -i \\\tadd_header X-Served-By 1574-web-01;' /etc/nginx/nginx.conf",
+  path    => '/usr/bin:/usr/sbin:/sbin:/bin',
+}
