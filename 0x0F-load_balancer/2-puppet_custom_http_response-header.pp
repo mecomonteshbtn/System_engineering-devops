0a1a612b05bd0ca3e37537477c84981f1d332141
Robinson Montes <1574@holbertonschool.com>
2020-08-15 02:07:36 +0000

    adding service in puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,8 +1,8 @@
 # Install and configure nginx on a ubuntu 16 server
 
 exec { 'apt-get update':
-  command => "apt-get update"
-  path    => "/usr/bin:/usr/sbin:/bin:/sbin",
+  command => 'apt-get update',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
 
 package { 'nginx':
@@ -11,6 +11,12 @@ package { 'nginx':
 }
 
 exec { 'custom_header':
-  command => "sed -i \\\tadd_header X-Served-By 1574-web-01;' /etc/nginx/nginx.conf",
+  command => 'sed -i \\\tadd_header X-Served-By 1574-web-01;' /etc/nginx/nginx.conf',
   path    => '/usr/bin:/usr/sbin:/sbin:/bin',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
 }
