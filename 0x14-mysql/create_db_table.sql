e72b49a9ede6d23b11004ea88d167a8544f5f464
Robinson Montes <1574@holbertonschool.com>
2020-09-03 21:59:55 +0000

    Create a database and a table with one entry

--- 0x14-mysql/create_db_table.sql
+++ 0x14-mysql/create_db_table.sql
@@ -3,3 +3,4 @@ CREATE DATABASE IF NOT EXISTS tyrell_corp;
 USE tyrell_corp;
 CREATE TABLE nexus6 (id INT, name VARCHAR(20));
 INSERT INTO nexus6 (id,name) VALUES(1,"Leon");
+GRANT SELECT ON nexus6 TO 'holberton_user'@'localhost'
