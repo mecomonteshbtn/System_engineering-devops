35c46f2a031ce8129699fed8ad55f7564d9c1e7d
Robinson Montes <1574@holbertonschool.com>
2020-09-14 23:28:29 +0000

    Export the data into csv file to a json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 16:22:14 2020
+
+@author: Robinson Montes
+"""
+from json import dump
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)
+    username = response.json().get('username')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
+    tasks = response.json()
+    dictionary = {user_id: []}
+    for task in tasks:
+        dictionary[user_id].append({
+                                    "task": task.get('title'),
+                                    "completed": task.get('completed'),
+                                    "username": username
+                                    })
+    with open('{}.json'.format(user_id), 'w') as file:
+        dump(dictionary, file)
