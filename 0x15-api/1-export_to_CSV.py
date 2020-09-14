8c84e4c7ceac2ad7ebfa82321860cf36fa26664a
Robinson Montes <1574@holbertonschool.com>
2020-09-14 22:46:37 +0000

    Export the information to a csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 16:22:14 2020
+
+@author: Robinson Montes
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)
+    username = response.json().get('username')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+    with open('{}.csv'.format(user_id), 'w') as file:
+        for task in tasks:
+            file.write("{}, {}, {}, {}\n"
+                       .format(user_id, username, task.get('completed'),
+                               task.get('title')))
