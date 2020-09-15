e71735550d3e12a969e40e17c4a47dceb76a9f89
Robinson Montes <1574@holbertonschool.com>
2020-09-15 00:45:25 +0000

    Create a json file with all the information baout users' tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 16:22:14 2020
+
+@author: Robinson Montes
+"""
+from json import dump
+from requests import get
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = get(url)
+    users = response.json()
+
+    dictionary = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        url = url + '/todos/'
+        response = get(url)
+        tasks = response.json()
+        dictionary[user_id] = []
+        for task in tasks:
+            dictionary[user_id].append({
+                                        "task": task.get('title'),
+                                        "completed": task.get('completed'),
+                                        "username": username
+                                        })
+    with open('todo_all_employees.json', 'w') as file:
+        dump(dictionary, file)
