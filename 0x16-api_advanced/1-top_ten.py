947e9bc4cbee5137f044cb59b20164209b3df92a
Robinson Montes <1574@holbertonschool.com>
2020-09-17 22:56:00 +0000

    fixing pep8 style in 1-top file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,10 +16,9 @@ def top_ten(subreddit):
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)',
-        allow_redirects = False
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-    response = get(url, headers=headers)
+    response = get(url, headers=headers, allow_redirects=False)
     reddits = response.json()
 
     try:
