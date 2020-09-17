a3419efa036481bfc408c2fa86015e9e61d966f4
Robinson Montes <1574@holbertonschool.com>
2020-09-17 22:51:43 +0000

    fixing pep8 style in 1-top file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Created on Thu Sep 17 11:47:53 2020
@@ -17,7 +16,8 @@ def top_ten(subreddit):
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)',
+        allow_redirects = False
     }
     response = get(url, headers=headers)
     reddits = response.json()
@@ -27,4 +27,4 @@ def top_ten(subreddit):
         for i in range(10):
             print(children[i].get('data').get('title'))
     except:
-        print(None)
+        print('None')
