9c4fff328f897421d06f048161d28545bba25dda
Robinson Montes <1574@holbertonschool.com>
2020-09-17 19:38:57 +0000

    a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+Created on Thu Sep 17 11:47:53 2020
+
+@author: Robinson Montes
+"""
+from json import loads
+from requests import get
+
+
+def top_ten(subreddit):
+    """queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/top.json'.format(subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = get(url, headers=headers)
+    reddits = response.json()
+
+    try:
+        children = reddits.get('data').get('children')
+    except:
+        print(None)
+        return
+    for i in range(10):
+        print(children[i].get('data').get('title'))
