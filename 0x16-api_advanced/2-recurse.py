55fd181cb06f19164a1885798a969bd75b896dd8
Robinson Montes <1574@holbertonschool.com>
2020-09-17 23:43:30 +0000

    forbiden redirections on 2-recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -20,7 +20,7 @@ def recurse(subreddit, hot_list=[]):
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-    response = get(url, headers=headers)
+    response = get(url, headers=headers, allow_redirects=False)
     reddits = response.json()
 
     try:
