a85eeb1585b42ff66434d08e9682e088f7ca8172
Robinson Montes <1574@holbertonschool.com>
2020-09-30 14:45:54 +0000

    formatting the file according to puppet lint style

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,5 @@
-exec { '/var/www/html/wp-setting.php':  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",}
+# Puppet script that fixes whole typo errors in php files
+exec { '/var/www/html/wp-setting.php':
+  path	  => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+}
