c0f28d5bf482af4b91c9d5d7d42ee507656c6b38
Robinson Montes <1574@holbertonschool.com>
2020-10-09 15:01:48 +0000

    upgrade the number of allowed requests for nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+# Fix the nginx requests limit
+exec { 'upgrade':
+  path    => '/bin/',
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
+}
+
+exec { 'restart':
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
+}
